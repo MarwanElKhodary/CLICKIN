2673076f2182c909fca1b4caa57b23afce01635b
Marwan ElKhodary <marwan.elkhodary@gmail.com>
2025-03-16 15:04:03 -0400

    feat: Add initial db migration

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,12 +4,17 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\CountRepository;
 
 class HomeController 
 {
     #[Route('/')]
-    public function index(): Response 
+    public function index(CountRepository $respository): Response 
     {
+        $count = $respository->findAll();
+
+        dd($count);
+
         return new Response("Hello from a controller!");
     }
 }
