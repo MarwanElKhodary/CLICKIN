633dd710ac16850e9d604bbb395e6995b76c2460
Marwan ElKhodary <marwan.elkhodary@gmail.com>
2025-03-25 01:03:53 -0400

    feat: Connect to mySQL with go

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+
+var db *sql.DB
 
 func main() {
-	fmt.Println("Hello World")
+	envErr := godotenv.Load(".env")
+	if envErr != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	//Capture connection properties.
+	cfg := mysql.Config{
+		User:   os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
+		DBName: "simple_sre_db",
+	}
+
+	//Get a database handle.
+	var dbErr error
+	db, dbErr = sql.Open("mysql", cfg.FormatDSN())
+	if dbErr != nil {
+		log.Fatal(dbErr)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
 }
