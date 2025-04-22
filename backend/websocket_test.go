b6409b31c1b61edc6bd875ae62c7e594f17be50c
Marwan ElKhodary <marwan.elkhodary@gmail.com>
2025-04-22 16:14:56 -0400

    feat: Add TestBroadcastCount

--- backend/websocket_test.go
+++ backend/websocket_test.go
@@ -4,9 +4,11 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
+	"sync"
 	"testing"
 
 	"github.com/gorilla/websocket"
@@ -35,4 +37,55 @@ func TestWebSocketConnection(t *testing.T) {
 	mutex.Lock()
 	assert.Equal(t, len(clients), 1, "One client should be connected")
 	mutex.Unlock()
+}
+
+// TestBroadcastCount tests the BroadcastCount function.
+// It verifies that when BroadcastCount is called, all connected clients
+// receive the updated count via WebSocket.
+func TestBroadcastCount(t *testing.T) {
+	teardownTestCase := setupTestCase(t)
+	defer teardownTestCase(t)
+
+	s := httptest.NewServer(http.HandlerFunc(wsHandler))
+	defer s.Close()
+
+	// Convert http://127.0.0.1 to ws://127.0.0.
+	wsURL := "ws" + strings.TrimPrefix(s.URL, "http")
+
+	conn1, _, err := websocket.DefaultDialer.Dial(wsURL, nil)
+	if err != nil {
+		t.Fatalf("Could not connect to WebSocket server: %v", err)
+	}
+	defer conn1.Close()
+
+	conn2, _, err := websocket.DefaultDialer.Dial(wsURL, nil)
+	if err != nil {
+		t.Fatalf("Could not connect to WebSocket server: %v", err)
+	}
+	defer conn2.Close()
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	testCount := 69
+	expectedMsg := fmt.Sprintf("<span id=\"counter\">%d</span>", testCount)
+
+	readMessage := func(conn *websocket.Conn) {
+		defer wg.Done()
+
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			t.Errorf("Error reading message: %v", err)
+			return
+		}
+
+		assert.Equal(t, expectedMsg, string(msg), "Client receive the correct broadcast message")
+	}
+
+	go readMessage(conn1)
+	go readMessage(conn2)
+
+	BroadcastCount(testCount)
+
+	wg.Wait()
 }
